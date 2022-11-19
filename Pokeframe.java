cfd59c0230f5c4d4b2da63a51b0f36ad58286ec5
SynnK <93355393+synnkfps@users.noreply.github.com>
2022-11-19 14:12:43 -0300

    Update Pokeframe.java

--- Pokeframe.java
+++ Pokeframe.java
@@ -1,62 +1,88 @@
-// Imports
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
 import javax.swing.*;
 import java.awt.*;
+import java.io.File;
 import java.util.Objects;
 
 public class Pokeframe extends JFrame {
-    // All the pokemons (i didnt made it manually btw)
     public static String[] pokemons = {null,"bulbasaur","ivysaur","venusaur","charmander","charmeleon","charizard","squirtle","wartortle","blastoise","caterpie","metapod","butterfree","weedle","kakuna","beedrill","pidgey","pidgeotto","pidgeot","rattata","raticate","spearow","fearow","ekans","arbok","pikachu","raichu","sandshrew","sandslash","nidoran-f","nidorina","nidoqueen","nidoran-m","nidorino","nidoking","clefairy","clefable","vulpix","ninetales","jigglypuff","wigglytuff","zubat","golbat","oddish","gloom","vileplume","paras","parasect","venonat","venomoth","diglett","dugtrio","meowth","persian","psyduck","golduck","mankey","primeape","growlithe","arcanine","poliwag","poliwhirl","poliwrath","abra","kadabra","alakazam","machop","machoke","machamp","bellsprout","weepinbell","victreebel","tentacool","tentacruel","geodude","graveler","golem","ponyta","rapidash","slowpoke","slowbro","magnemite","magneton","farfetchd","doduo","dodrio","seel","dewgong","grimer","muk","shellder","cloyster","gastly","haunter","gengar","onix","drowzee","hypno","krabby","kingler","voltorb","electrode","exeggcute","exeggutor","cubone","marowak","hitmonlee","hitmonchan","lickitung","koffing","weezing","rhyhorn","rhydon","chansey","tangela","kangaskhan","horsea","seadra","goldeen","seaking","staryu","starmie","mr-mime","scyther","jynx","electabuzz","magmar","pinsir","tauros","magikarp","gyarados","lapras","ditto","eevee","vaporeon","jolteon","flareon","porygon","omanyte","omastar","kabuto","kabutops","aerodactyl","snorlax","articuno","zapdos","moltres","dratini","dragonair","dragonite","mewtwo"};
-    ImageIcon poke = new ImageIcon(Objects.requireNonNull(getClass().getResource("pokedex.jpg")));
+    ImageIcon poke = new ImageIcon(Objects.requireNonNull(getClass().getResource("pokedex_semcaracteres.jpg")));
     JLabel apa = new JLabel(poke);
-    
-    // Content
     JButton next = Botao(">", 280, 438-38, 40, 30);
+
     JButton previous = Botao("<", 220, 438-38, 40, 30);
     JButton ligar = Botao("ligar", 87, 415-38, 60, 16);
     JButton desligar = Botao("desligar", 156, 415-38, 60, 16);
     JButton plus10 = Botao("^", 256, 408-38, 30, 40);
+    JButton tocar = Botao("", 35, 404-38, 40, 40);
     JButton minus10 = Botao("v", 256, 458-38, 30, 40);
 
     static JLabel contador = Texto("0", 115, 443-38, 90, 90);
+    public static Boolean ligado = true;
     public static Integer count = Integer.parseInt(contador.getText());
 
     static JLabel pokemon = new JLabel(); // variavel para colocar o icone (imagem do pokemon)
     static JLabel atual = Texto("Nenhum", 480, 160, 100, 50);
 
-    // Main stuff
     public Pokeframe() {
-        
         setLayout(null);
+        // System.out.println(pokemons.toString());
         contador.setFont(new Font("Verdana", Font.PLAIN, 34));
         updateIcon();
-        
-        // Listeners
-        next.addActionListener(e -> {
-            modify(1);
-            contador.setText(String.valueOf(count));
-            updateIcon();
+        desligar.addActionListener(e -> {
+            ligado = false;
+            pokemon.setVisible(false);
+            contador.setVisible(false);
+            atual.setVisible(false);
         });
-        previous.addActionListener(e -> {
-            modify(-1);
-            contador.setText(String.valueOf(count));
-            updateIcon();
-        });
-        plus10.addActionListener(e -> {
-            modify(10);
-            contador.setText(String.valueOf(count));
-            updateIcon();
-        });
-        minus10.addActionListener(e -> {
-            modify(-10);
-            contador.setText(String.valueOf(count));
-            updateIcon();
+        ligar.addActionListener(e -> {
+            ligado = true;
+            pokemon.setVisible(true);
+            contador.setVisible(true);
+            atual.setVisible(true);
         });
 
-        // Images's bounds
+
+        next.addActionListener(e -> {
+            if (ligado) {
+                modify(1);
+                contador.setText(String.valueOf(count));
+                updateIcon();
+            }
+        });
+        previous.addActionListener(e -> {
+            if (ligado) {
+                modify(-1);
+                contador.setText(String.valueOf(count));
+                updateIcon();
+            }
+        });
+        plus10.addActionListener(e -> {
+            if (ligado) {
+                modify(10);
+                contador.setText(String.valueOf(count));
+                updateIcon();
+            }
+        });
+        minus10.addActionListener(e -> {
+            if (ligado) {
+                modify(-10);
+                contador.setText(String.valueOf(count));
+                updateIcon();
+            }
+        });
+        tocar.addActionListener(e -> {
+            if (ligado) {
+                tocarSom(count);
+            }
+        });
+
         pokemon.setBounds(80, 27, 400, 400);
         apa.setBounds(0,-38,700,590);
 
-        // Buttons
+        add(tocar);
         add(contador);
         add(plus10);
         add(minus10);
@@ -65,10 +91,9 @@ public class Pokeframe extends JFrame {
         add(ligar);
         add(desligar);
         add(pokemon);
-        add(atual); // pokemon name
+        add(atual);
         add(apa);
 
-        // Frame stuff
         setTitle("Pokedex");
         setSize(700,590-38);
         setDefaultCloseOperation(Pokeframe.EXIT_ON_CLOSE);
@@ -76,13 +101,9 @@ public class Pokeframe extends JFrame {
         setLocationRelativeTo(null);
         setVisible(true);
     }
-    
-    // Initialize
     public static void main(String[] arg) {
         new Pokeframe();
     }
-    
-    // Function to make the switching easier 
     public void modify(Integer value) {
         if (value < 0) {
             if (!(count + value < 0)) {
@@ -95,13 +116,25 @@ public class Pokeframe extends JFrame {
             if (!(count + value > 151)) {
                 count += value;
                 atual.setText(pokemons[count]);
+                // JOptionPane.showMessageDialog(null, PokemonStorage.getFromId(count));
             } else {
                 JOptionPane.showMessageDialog(null, "O ID não pode ser acima de 151");
             }
         }
     }
-    
-    // Update Icon function (to change the pokemon image)
+
+    public static void tocarSom(Integer indice) {
+        if (count!=0) {
+            try {
+                AudioInputStream audioInputStream = AudioSystem.getAudioInputStream(new File("pokesom/" + indice + ".wav").getAbsoluteFile());
+                Clip clip = AudioSystem.getClip();
+                clip.open(audioInputStream);
+                clip.start();
+            } catch (Exception ex) {
+                ex.printStackTrace();
+            }
+        }
+    }
     public static void updateIcon() {
         ImageIcon imageIcon = new ImageIcon("./pokemons/" + count + ".png");
         Image image = imageIcon.getImage();
@@ -110,11 +143,11 @@ public class Pokeframe extends JFrame {
         Image new_image = image.getScaledInstance((int) size_x, (int) size_y, Image.SCALE_FAST);
         imageIcon = new ImageIcon(new_image);
         pokemon.setIcon(imageIcon);
+        tocarSom(count);
     }
-    
-    // Button function to create buttons in a easier way
     public JButton Botao(String texto, Integer x, Integer y, Integer largura, Integer altura) {
         JButton temp = new JButton(texto);
+        //setLayout(null);
         temp.setBorder(null);
         temp.setFocusPainted(false);
         temp.setContentAreaFilled(false);
@@ -123,7 +156,6 @@ public class Pokeframe extends JFrame {
         return temp;
     }
 
-    // Label function to create labels in a easier way
     public static JLabel Texto(String texto, Integer x, Integer y, Integer largura, Integer altura) {
         JLabel temp = new JLabel(texto);
         temp.setBorder(null);
