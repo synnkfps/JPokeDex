7f4989fb6306f36fde19fe388bd1c45005654e0c
SynnK <93355393+synnkfps@users.noreply.github.com>
2022-12-03 14:45:54 -0300

    Update Pokeframe.java

--- Pokeframe.java
+++ Pokeframe.java
@@ -3,86 +3,308 @@ import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.Clip;
 import javax.swing.*;
 import java.awt.*;
-import java.io.File;
-import java.util.Objects;
+import java.io.*;
 
 public class Pokeframe extends JFrame {
-    public static String[] pokemons = {null,"bulbasaur","ivysaur","venusaur","charmander","charmeleon","charizard","squirtle","wartortle","blastoise","caterpie","metapod","butterfree","weedle","kakuna","beedrill","pidgey","pidgeotto","pidgeot","rattata","raticate","spearow","fearow","ekans","arbok","pikachu","raichu","sandshrew","sandslash","nidoran-f","nidorina","nidoqueen","nidoran-m","nidorino","nidoking","clefairy","clefable","vulpix","ninetales","jigglypuff","wigglytuff","zubat","golbat","oddish","gloom","vileplume","paras","parasect","venonat","venomoth","diglett","dugtrio","meowth","persian","psyduck","golduck","mankey","primeape","growlithe","arcanine","poliwag","poliwhirl","poliwrath","abra","kadabra","alakazam","machop","machoke","machamp","bellsprout","weepinbell","victreebel","tentacool","tentacruel","geodude","graveler","golem","ponyta","rapidash","slowpoke","slowbro","magnemite","magneton","farfetchd","doduo","dodrio","seel","dewgong","grimer","muk","shellder","cloyster","gastly","haunter","gengar","onix","drowzee","hypno","krabby","kingler","voltorb","electrode","exeggcute","exeggutor","cubone","marowak","hitmonlee","hitmonchan","lickitung","koffing","weezing","rhyhorn","rhydon","chansey","tangela","kangaskhan","horsea","seadra","goldeen","seaking","staryu","starmie","mr-mime","scyther","jynx","electabuzz","magmar","pinsir","tauros","magikarp","gyarados","lapras","ditto","eevee","vaporeon","jolteon","flareon","porygon","omanyte","omastar","kabuto","kabutops","aerodactyl","snorlax","articuno","zapdos","moltres","dratini","dragonair","dragonite","mewtwo"};
-    ImageIcon poke = new ImageIcon(Objects.requireNonNull(getClass().getResource("pokedex_semcaracteres.jpg")));
+    ImageIcon poke = new ImageIcon(getClass().getResource("pokedex_semcaracteres.jpg"));
     JLabel apa = new JLabel(poke);
-    JButton next = Botao(">", 280, 438-38, 40, 30);
-
-    JButton previous = Botao("<", 220, 438-38, 40, 30);
+    JButton next = Botao("->", 280, 438-38, 40, 30);
+    JButton previous = Botao("<-", 220, 438-38, 40, 30);
     JButton ligar = Botao("ligar", 87, 415-38, 60, 16);
     JButton desligar = Botao("desligar", 156, 415-38, 60, 16);
     JButton plus10 = Botao("^", 256, 408-38, 30, 40);
-    JButton tocar = Botao("", 35, 404-38, 40, 40);
     JButton minus10 = Botao("v", 256, 458-38, 30, 40);
+    JButton audio = Botao("", 40, 370, 40, 30);
+    JButton um = Botao("1", 440, 275, 40, 25);
+    JButton dois = Botao("2", 485, 275, 32, 25);
+    JButton tres = Botao("3", 523, 275, 32, 25);
+    JButton quatro = Botao("4", 560, 275, 35, 25);
+    JButton cinco = Botao("5", 600, 275, 35, 25);
+    JButton seis = Botao("6", 440, 304, 40, 25);
+    JButton sete = Botao("7", 485, 304, 32, 25);
+    JButton oito = Botao("8", 523, 304, 32, 25);
+    JButton nove = Botao("9", 560, 304, 35, 25);
+    JButton zero = Botao("0", 600, 304, 35, 25);
+    JButton enter = Botao("", 620, 375, 25, 25);
+
+    String scount;
+    int ordem;
 
     static JLabel contador = Texto("0", 115, 443-38, 90, 90);
+    static JLabel descricao = Texto("0", 115, 443-38, 90, 90);
     public static Boolean ligado = true;
-    public static Integer count = Integer.parseInt(contador.getText());
 
+    public static Integer count = Integer.parseInt(contador.getText());
+    static JLabel todosOsStats = Texto("Nenhum", 495, -43, 500, 500);
     static JLabel pokemon = new JLabel(); // variavel para colocar o icone (imagem do pokemon)
-    static JLabel atual = Texto("Nenhum", 480, 160, 100, 50);
 
     public Pokeframe() {
         setLayout(null);
-        // System.out.println(pokemons.toString());
         contador.setFont(new Font("Verdana", Font.PLAIN, 34));
+        todosOsStats.setFont(new Font("Verdana", Font.PLAIN, 9));
         updateIcon();
+
+        um.addActionListener(e -> {
+            if (contador.getText().length() <= 2) {
+                if (ordem >= 0 && ordem < 16) {
+                    scount = ""+ordem+1;
+                    ordem = Integer.parseInt(scount);
+                    contador.setText(scount);
+                }else {
+                    ordem = 1;
+                    scount = ""+ordem;
+                    ordem = Integer.parseInt(scount);
+                    contador.setText(scount);
+                }
+            }else  {
+                ordem = 1;
+                scount = ""+ordem;
+                ordem = Integer.parseInt(scount);
+                contador.setText(scount);
+            }
+        });
+
+        dois.addActionListener(e -> {
+            if (contador.getText().length() <= 2) {
+                if (ordem >= 0 && ordem < 15) {
+                    scount = ""+ordem+2;
+                    ordem = Integer.parseInt(scount);
+                    contador.setText(scount);
+                }else {
+                    ordem = 2;
+                    scount = ""+ordem;
+                    ordem = Integer.parseInt(scount);
+                    contador.setText(scount);
+                }
+            }else  {
+                ordem = 2;
+                scount = ""+ordem;
+                ordem = Integer.parseInt(scount);
+                contador.setText(scount);
+            }
+        });
+
+        tres.addActionListener(e -> {
+            if (contador.getText().length() <= 2) {
+                if (ordem >= 0 && ordem < 15) {
+                    scount = ""+ordem+3;
+                    ordem = Integer.parseInt(scount);
+                    contador.setText(scount);
+                }else {
+                    ordem = 3;
+                    scount = ""+ordem;
+                    ordem = Integer.parseInt(scount);
+                    contador.setText(scount);
+                }
+            }else  {
+                ordem = 3;
+                scount = ""+ordem;
+                ordem = Integer.parseInt(scount);
+                contador.setText(scount);
+            }
+        });
+
+        quatro.addActionListener(e -> {
+            if (contador.getText().length() <= 2) {
+                if (ordem >= 0 && ordem < 15) {
+                    scount = ""+ordem+4;
+                    ordem = Integer.parseInt(scount);
+                    contador.setText(scount);
+                }else {
+                    ordem = 4;
+                    scount = ""+ordem;
+                    ordem = Integer.parseInt(scount);
+                    contador.setText(scount);
+                }
+            }else  {
+                ordem = 4;
+                scount = ""+ordem;
+                ordem = Integer.parseInt(scount);
+                contador.setText(scount);
+            }
+        });
+
+        cinco.addActionListener(e -> {
+            if (contador.getText().length() <= 2) {
+                if (ordem >= 0 && ordem < 15) {
+                    scount = ""+ordem+5;
+                    ordem = Integer.parseInt(scount);
+                    contador.setText(scount);
+                }else {
+                    ordem = 5;
+                    scount = ""+ordem;
+                    ordem = Integer.parseInt(scount);
+                    contador.setText(scount);
+                }
+            }else  {
+                ordem = 5;
+                scount = ""+ordem;
+                ordem = Integer.parseInt(scount);
+                contador.setText(scount);
+            }
+        });
+
+        seis.addActionListener(e -> {
+            if (contador.getText().length() <= 2) {
+                if (ordem >= 0 && ordem < 15) {
+                    scount = ""+ordem+6;
+                    ordem = Integer.parseInt(scount);
+                    contador.setText(scount);
+                }else {
+                    ordem = 6;
+                    scount = ""+ordem;
+                    ordem = Integer.parseInt(scount);
+                    contador.setText(scount);
+                }
+            }else  {
+                ordem = 6;
+                scount = ""+ordem;
+                ordem = Integer.parseInt(scount);
+                contador.setText(scount);
+            }
+        });
+
+        sete.addActionListener(e -> {
+            if (contador.getText().length() <= 2) {
+                if (ordem >= 0 && ordem < 15) {
+                    scount = ""+ordem+7;
+                    ordem = Integer.parseInt(scount);
+                    contador.setText(scount);
+                }else {
+                    ordem = 7;
+                    scount = ""+ordem;
+                    ordem = Integer.parseInt(scount);
+                    contador.setText(scount);
+                }
+            }else  {
+                ordem = 7;
+                scount = ""+ordem;
+                ordem = Integer.parseInt(scount);
+                contador.setText(scount);
+            }
+        });
+
+        oito.addActionListener(e -> {
+            if (contador.getText().length() <= 2) {
+                if (ordem >= 0 && ordem < 15) {
+                    scount = ""+ordem+8;
+                    ordem = Integer.parseInt(scount);
+                    contador.setText(scount);
+                }else {
+                    ordem = 8;
+                    scount = ""+ordem;
+                    ordem = Integer.parseInt(scount);
+                    contador.setText(scount);
+                }
+            }else  {
+                ordem = 8;
+                scount = ""+ordem;
+                ordem = Integer.parseInt(scount);
+                contador.setText(scount);
+            }
+        });
+
+        nove.addActionListener(e -> {
+            if (contador.getText().length() <= 2) {
+                if (ordem >= 0 && ordem < 15) {
+                    scount = ""+ordem+9;
+                    ordem = Integer.parseInt(scount);
+                    contador.setText(scount);
+                }else {
+                    ordem = 9;
+                    scount = ""+ordem;
+                    ordem = Integer.parseInt(scount);
+                    contador.setText(scount);
+                }
+            }else  {
+                ordem = 9;
+                scount = ""+ordem;
+                ordem = Integer.parseInt(scount);
+                contador.setText(scount);
+            }
+        });
+
+        // Sistema de liga e desliga
+        ligar.addActionListener(e -> {
+            ligado=true;
+            contador.setVisible(true);
+            pokemon.setVisible(true);
+            todosOsStats.setVisible(true);
+        });
         desligar.addActionListener(e -> {
             ligado = false;
-            pokemon.setVisible(false);
             contador.setVisible(false);
-            atual.setVisible(false);
-        });
-        ligar.addActionListener(e -> {
-            ligado = true;
-            pokemon.setVisible(true);
-            contador.setVisible(true);
-            atual.setVisible(true);
+            pokemon.setVisible(false);
+            todosOsStats.setVisible(false);
         });
 
+        zero.addActionListener(e -> {
+            if (contador.getText().length() <= 2) {
+                if (ordem >= 0 && ordem < 16) {
+                    scount = ""+ordem+0;
+                    ordem = Integer.parseInt(scount);
+                    contador.setText(scount);
+                }else {
+                    ordem = 0;
+                    scount = ""+ordem;
+                    ordem = Integer.parseInt(scount);
+                    contador.setText(scount);
+                }
+            }else  {
+                ordem = 0;
+                scount = ""+ordem;
+                ordem = Integer.parseInt(scount);
+                contador.setText(scount);
+            }
+        });
+
+        enter.addActionListener(e -> {
+            if (ordem < 0 || ordem>151) {
+                ordem = 0;
+
+            } else {
+                count = Integer.parseInt(scount);
+                contador.setText(String.valueOf(count));
+                pegarStatus(count);
+                updateIcon();
+                ordem = 0;
+            }
+        });
 
         next.addActionListener(e -> {
-            if (ligado) {
-                modify(1);
-                contador.setText(String.valueOf(count));
-                updateIcon();
+            modify(1);
+            contador.setText(String.valueOf(count));
+            updateIcon();
+        });
+        audio.addActionListener(e -> {
+            if (!ligado) return;
+            contador.setText(String.valueOf(count));
+            updatesom();
+            if(count == 0 || count > 151) {
+                JOptionPane.showMessageDialog(null, "Pokemon Inexistente");
             }
         });
         previous.addActionListener(e -> {
-            if (ligado) {
-                modify(-1);
-                contador.setText(String.valueOf(count));
-                updateIcon();
-            }
+            modify(-1);
+            contador.setText(String.valueOf(count));
+            updateIcon();
         });
+
         plus10.addActionListener(e -> {
-            if (ligado) {
-                modify(10);
-                contador.setText(String.valueOf(count));
-                updateIcon();
-            }
+            modify(10);
+            contador.setText(String.valueOf(count));
+            updateIcon();
         });
         minus10.addActionListener(e -> {
-            if (ligado) {
-                modify(-10);
-                contador.setText(String.valueOf(count));
-                updateIcon();
-            }
-        });
-        tocar.addActionListener(e -> {
-            if (ligado) {
-                tocarSom(count);
-            }
+            modify(-10);
+            contador.setText(String.valueOf(count));
+            updateIcon();
         });
 
         pokemon.setBounds(80, 27, 400, 400);
         apa.setBounds(0,-38,700,590);
 
-        add(tocar);
+        add(todosOsStats);
         add(contador);
         add(plus10);
         add(minus10);
@@ -91,60 +313,88 @@ public class Pokeframe extends JFrame {
         add(ligar);
         add(desligar);
         add(pokemon);
-        add(atual);
-        add(apa);
+        add(audio);
+        add(um);
+        add(dois);
+        add(tres);
+        add(quatro);
+        add(cinco);
+        add(seis);
+        add(sete);
+        add(oito);
+        add(nove);
+        add(zero);
+        add(enter);
 
-        setTitle("Pokedex");
+        add(apa);
+        setTitle("Pokedexzinho");
         setSize(700,590-38);
-        setDefaultCloseOperation(Pokeframe.EXIT_ON_CLOSE);
-        setResizable(true);
+        setResizable(false);
         setLocationRelativeTo(null);
         setVisible(true);
     }
     public static void main(String[] arg) {
         new Pokeframe();
     }
+
     public void modify(Integer value) {
+        if (!ligado) return;
         if (value < 0) {
             if (!(count + value < 0)) {
                 count += value;
-                atual.setText(pokemons[count]);
+                pegarStatus(count);
             } else {
                 JOptionPane.showMessageDialog(null, "O ID não pode ser abaixo de 0");
             }
         } else {
             if (!(count + value > 151)) {
                 count += value;
-                atual.setText(pokemons[count]);
-                // JOptionPane.showMessageDialog(null, PokemonStorage.getFromId(count));
+                pegarStatus(count);
             } else {
                 JOptionPane.showMessageDialog(null, "O ID não pode ser acima de 151");
             }
         }
     }
 
-    public static void tocarSom(Integer indice) {
-        if (count!=0) {
-            try {
-                AudioInputStream audioInputStream = AudioSystem.getAudioInputStream(new File("pokesom/" + indice + ".wav").getAbsoluteFile());
-                Clip clip = AudioSystem.getClip();
-                clip.open(audioInputStream);
-                clip.start();
-            } catch (Exception ex) {
-                ex.printStackTrace();
+    public static void pegarStatus(Integer id) {
+        todosOsStats.setText("");
+        try (BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream(Pokeframe.class.getResource("status/" +id+ ".txt").getFile()), "Cp1250"))) {
+            String line = null;
+            todosOsStats.setText("<html>");
+            while ((line = br.readLine()) != null) {
+                todosOsStats.setText(todosOsStats.getText() + line + "<br>");
+                System.out.println(todosOsStats.getText());
             }
+            todosOsStats.setText(todosOsStats.getText()+"</html>");
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
+
+
     public static void updateIcon() {
-        ImageIcon imageIcon = new ImageIcon("./pokemons/" + count + ".png");
+        ImageIcon imageIcon = new ImageIcon(Pokeframe.class.getResource(  "pokemons/" + count +  ".png" ));
         Image image = imageIcon.getImage();
         float size_x = imageIcon.getIconWidth()*2f;
         float size_y = imageIcon.getIconHeight()*2f;
         Image new_image = image.getScaledInstance((int) size_x, (int) size_y, Image.SCALE_FAST);
         imageIcon = new ImageIcon(new_image);
         pokemon.setIcon(imageIcon);
-        tocarSom(count);
     }
+
+    public static void updatesom() {
+        try {
+            AudioInputStream audioInputStream = AudioSystem.getAudioInputStream(new File("poke_som/" + count + ".wav").getAbsoluteFile());
+            Clip clip = AudioSystem.getClip();
+            clip.open(audioInputStream);
+            clip.start();
+        } catch (Exception ex) {
+            System.out.println("Erro ao executar SOM!");
+            ex.printStackTrace();
+        }
+    }
+
+
     public JButton Botao(String texto, Integer x, Integer y, Integer largura, Integer altura) {
         JButton temp = new JButton(texto);
         //setLayout(null);
