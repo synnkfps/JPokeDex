6268353b5e4b27d4235b34a7219356cb3bebfa14
SynnK <93355393+synnkfps@users.noreply.github.com>
2022-12-10 09:38:35 -0300

    Update Pokeframe.java

--- Pokeframe.java
+++ Pokeframe.java
@@ -6,8 +6,10 @@ import java.awt.*;
 import java.io.*;
 
 public class Pokeframe extends JFrame {
+    // Imagem de Fundo
     ImageIcon poke = new ImageIcon(getClass().getResource("pokedex_semcaracteres.jpg"));
     JLabel apa = new JLabel(poke);
+    // Botões
     JButton next = Botao("->", 280, 438-38, 40, 30);
     JButton previous = Botao("<-", 220, 438-38, 40, 30);
     JButton ligar = Botao("ligar", 87, 415-38, 60, 16);
@@ -15,6 +17,7 @@ public class Pokeframe extends JFrame {
     JButton plus10 = Botao("^", 256, 408-38, 30, 40);
     JButton minus10 = Botao("v", 256, 458-38, 30, 40);
     JButton audio = Botao("", 40, 370, 40, 30);
+    // Números
     JButton um = Botao("1", 440, 275, 40, 25);
     JButton dois = Botao("2", 485, 275, 32, 25);
     JButton tres = Botao("3", 523, 275, 32, 25);
@@ -34,8 +37,8 @@ public class Pokeframe extends JFrame {
     static JLabel descricao = Texto("0", 115, 443-38, 90, 90);
     public static Boolean ligado = true;
 
-    public static Integer count = Integer.parseInt(contador.getText());
-    static JLabel todosOsStats = Texto("Nenhum", 495, -43, 500, 500);
+    public static Integer count = Integer.parseInt(contador.getText()); // Variável matriarca
+    static JLabel todosOsStats = Texto("Nenhum", 495, -43, 500, 500); // Nenhum vai representar o 0
     static JLabel pokemon = new JLabel(); // variavel para colocar o icone (imagem do pokemon)
 
     public Pokeframe() {
@@ -44,6 +47,7 @@ public class Pokeframe extends JFrame {
         todosOsStats.setFont(new Font("Verdana", Font.PLAIN, 9));
         updateIcon();
 
+        // Números, cada número tem suas funções para trocar a imagem, o som e o status
         um.addActionListener(e -> {
             if (contador.getText().length() <= 2) {
                 if (ordem >= 0 && ordem < 16) {
@@ -271,11 +275,20 @@ public class Pokeframe extends JFrame {
             }
         });
 
+        // Next (próximo), colocar o próximo pokémon
         next.addActionListener(e -> {
             modify(1);
             contador.setText(String.valueOf(count));
             updateIcon();
         });
+        // Previous (anterior), colocar o pokémon anterior
+        previous.addActionListener(e -> {
+            modify(-1);
+            contador.setText(String.valueOf(count));
+            updateIcon();
+        });
+        
+        // Botão para tocar o áudio
         audio.addActionListener(e -> {
             if (!ligado) return;
             contador.setText(String.valueOf(count));
@@ -284,26 +297,25 @@ public class Pokeframe extends JFrame {
                 JOptionPane.showMessageDialog(null, "Pokemon Inexistente");
             }
         });
-        previous.addActionListener(e -> {
-            modify(-1);
-            contador.setText(String.valueOf(count));
-            updateIcon();
-        });
-
+   
+        // Mais 10
         plus10.addActionListener(e -> {
             modify(10);
             contador.setText(String.valueOf(count));
             updateIcon();
         });
+        // Menos 10
         minus10.addActionListener(e -> {
             modify(-10);
             contador.setText(String.valueOf(count));
             updateIcon();
         });
 
+        // Posição da imagem do pokémon e do background
         pokemon.setBounds(80, 27, 400, 400);
         apa.setBounds(0,-38,700,590);
 
+        // Adicionar tudo
         add(todosOsStats);
         add(contador);
         add(plus10);
@@ -325,21 +337,27 @@ public class Pokeframe extends JFrame {
         add(nove);
         add(zero);
         add(enter);
-
         add(apa);
+        
+        // Definições da Janela (texto, tamanho, redimensionável, posição, visibilidade)
         setTitle("Pokedexzinho");
         setSize(700,590-38);
         setResizable(false);
         setLocationRelativeTo(null);
         setVisible(true);
     }
+    
+    // Função main (principal): Inicializa o frame inteiro
     public static void main(String[] arg) {
         new Pokeframe();
     }
 
+    // Função modify(valor): função que é responsável pelo funcionamento geral do programa
+    // Basicamente ela tem um parâmetro que é "value", o valor/ID do pokémon atual (que é a variável count)
     public void modify(Integer value) {
         if (!ligado) return;
         if (value < 0) {
+            // Prediction, vê se a próxima ação do usuário vai resultar em um valor abaixo de zero
             if (!(count + value < 0)) {
                 count += value;
                 pegarStatus(count);
@@ -347,6 +365,7 @@ public class Pokeframe extends JFrame {
                 JOptionPane.showMessageDialog(null, "O ID não pode ser abaixo de 0");
             }
         } else {
+            // Prediction, vê se a próxima ação do usuário vai resultar em um valor abaixo de zero
             if (!(count + value > 151)) {
                 count += value;
                 pegarStatus(count);
@@ -356,22 +375,26 @@ public class Pokeframe extends JFrame {
         }
     }
 
+    // Função pegarStatus(id): pega o status do ID chamado
+    // Utiliza funções de html pois o java não aceita \n (quebra de linha) em um JLabel, já com HTML é só usar o <br>.
     public static void pegarStatus(Integer id) {
         todosOsStats.setText("");
         try (BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream(Pokeframe.class.getResource("status/" +id+ ".txt").getFile()), "Cp1250"))) {
             String line = null;
             todosOsStats.setText("<html>");
+            // Ler todas as linhas
             while ((line = br.readLine()) != null) {
                 todosOsStats.setText(todosOsStats.getText() + line + "<br>");
                 System.out.println(todosOsStats.getText());
             }
+            // Após ler, colocar um </html> para fechar
             todosOsStats.setText(todosOsStats.getText()+"</html>");
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
-
+    // função updateIcon(): altera o ícone pro valor atual do contador
     public static void updateIcon() {
         ImageIcon imageIcon = new ImageIcon(Pokeframe.class.getResource(  "pokemons/" + count +  ".png" ));
         Image image = imageIcon.getImage();
@@ -382,6 +405,7 @@ public class Pokeframe extends JFrame {
         pokemon.setIcon(imageIcon);
     }
 
+    // função updatesom(): toca o som pro id atual
     public static void updatesom() {
         try {
             AudioInputStream audioInputStream = AudioSystem.getAudioInputStream(new File("poke_som/" + count + ".wav").getAbsoluteFile());
@@ -394,7 +418,7 @@ public class Pokeframe extends JFrame {
         }
     }
 
-
+    // função Botao: função para facilitar a criação de botão
     public JButton Botao(String texto, Integer x, Integer y, Integer largura, Integer altura) {
         JButton temp = new JButton(texto);
         //setLayout(null);
@@ -406,6 +430,7 @@ public class Pokeframe extends JFrame {
         return temp;
     }
 
+    // função Texto: função para facilitar a criação de texto
     public static JLabel Texto(String texto, Integer x, Integer y, Integer largura, Integer altura) {
         JLabel temp = new JLabel(texto);
         temp.setBorder(null);
